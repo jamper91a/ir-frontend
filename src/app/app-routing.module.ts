91b7434c0788d04ccee073d11fe3000a38f2c16f
jamper91a <jamper91a@gmal.com>
2019-12-16 19:20:35 +1300

    Subroutes added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+/* tslint:disable:max-line-length */
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import {SuperAdminGuard} from '../guards/superAdmin.guard';
@@ -26,7 +27,34 @@ const routes: Routes = [
     canActivate: [DealerGuard],
     loadChildren: './dealer/home/home.module#HomePageModule'
   },
-  { path: 'erp', loadChildren: './admin/erp/erp.module#ErpPageModule' }
+
+    // SuperAdmin
+  { path: 'superAdmin/dealers/create', loadChildren: './superAdmin/dealers/dealers-create/dealers.module#DealersPageModule' },
+  { path: 'superAdmin/dealers/details/:id', loadChildren: './superAdmin/dealers/dealers-detail/dealers-detail.module#DealersDetailPageModule' },
+  { path: 'superAdmin/dealers/list', loadChildren: './superAdmin/dealers/dealers-list/dealers-list.module#DealersListPageModule' },
+    // Admin
+  { path: 'admin/company', loadChildren: './admin/company/company.module#CompanyPageModule' },
+  { path: 'admin/erp', loadChildren: './admin/erp/erp.module#ErpPageModule' },
+  { path: 'admin/products', loadChildren: './admin/products/products.module#ProductsPageModule' },
+  { path: 'admin/products/create', loadChildren: './admin/products/product-create/product-create.module#ProductCreatePageModule' },
+  { path: 'admin/products/import', loadChildren: './admin/products/products-import/products-import.module#ProductsImportPageModule' },
+  { path: 'admin/shops', loadChildren: './admin/shops/shops.module#ShopsPageModule' },
+  { path: 'admin/shops/edit/:id', loadChildren: './admin/shops/edit-shop/edit-shop.module#EditShopPageModule' },
+  { path: 'admin/suppliers', loadChildren: './admin/suppliers/suppliers.module#SuppliersPageModule' },
+  { path: 'admin/suppliers/create', loadChildren: './admin/suppliers/supplier-create/supplier-create.module#SupplierCreatePageModule' },
+  { path: 'admin/suppliers/edit/:id', loadChildren: './admin/suppliers/edit-supplier/edit-supplier.module#EditSupplierPageModule' },
+  { path: 'admin/users', loadChildren: './admin/users/users.module#UsersPageModule' },
+  { path: 'admin/users/edit/:id', loadChildren: './admin/users/edit-user/edit-user.module#EditUserPageModule' },
+  { path: 'admin/zones', loadChildren: './admin/zones/zones.module#ZonesPageModule' },
+  { path: 'admin/zones/create/:id', loadChildren: './admin/zones/create-zone/create-zone.module#CreateZonePageModule' },
+  { path: 'admin/zones/edit/:id', loadChildren: './admin/zones/edit-zone/edit-zone.module#EditZonePageModule' },
+    // Dealer
+  { path: 'dealer/companies/create', loadChildren: './dealer/companies/companies-create/companies-create.module#CompaniesCreatePageModule' },
+  { path: 'dealer/companies/list', loadChildren: './dealer/companies/companies-list/companies-list.module#CompaniesListPageModule' },
+  { path: 'dealer/companies/details/:id', loadChildren: './dealer/companies/companies-details/companies-details.module#CompaniesDetailsPageModule' },
+  { path: 'dealer/tags/create/step-1', loadChildren: './dealer/tags/tags-create/tags-create.module#TagsCreatePageModule' },
+  { path: 'dealer/tags/create/step-2/:id', loadChildren: './dealer/tags/tags-create-step-two/tags-create-step-two.module#TagsCreateStepTwoPageModule' },
+  { path: 'dealer/reports', loadChildren: './dealer/reports/reports.module#ReportsPageModule' }
 ];
 
 @NgModule({
